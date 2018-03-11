ae49e93af5ca2fc0d576d0b10325d7b48f4bbd9e
Shannon <13708207+smckinney1@users.noreply.github.com>
2018-03-11 17:16:07 -0400

    Cleaned up App.js file by adding components for the different bookshelves.

--- src/WantToRead.js
+++ src/WantToRead.js
@@ -1,16 +1,16 @@
 import React, { Component } from 'react'
 
-class CurrentlyReading extends Component {
+class WantToRead extends Component {
     render() {
         return (
             <div className="bookshelf">
-            <h2 className="bookshelf-title">Currently Reading</h2>
+            <h2 className="bookshelf-title">Want to Read</h2>
             <div className="bookshelf-books">
               <ol className="books-grid">
                 <li>
                   <div className="book">
                     <div className="book-top">
-                      <div className="book-cover" style={{ width: 128, height: 193, backgroundImage: 'url("http://books.google.com/books/content?id=PGR2AwAAQBAJ&printsec=frontcover&img=1&zoom=1&imgtk=AFLRE73-GnPVEyb7MOCxDzOYF1PTQRuf6nCss9LMNOSWBpxBrz8Pm2_mFtWMMg_Y1dx92HT7cUoQBeSWjs3oEztBVhUeDFQX6-tWlWz1-feexS0mlJPjotcwFqAg6hBYDXuK_bkyHD-y&source=gbs_api")' }}></div>
+                      <div className="book-cover" style={{ width: 128, height: 193, backgroundImage: 'url("http://books.google.com/books/content?id=uu1mC6zWNTwC&printsec=frontcover&img=1&zoom=1&imgtk=AFLRE73pGHfBNSsJG9Y8kRBpmLUft9O4BfItHioHolWNKOdLavw-SLcXADy3CPAfJ0_qMb18RmCa7Ds1cTdpM3dxAGJs8zfCfm8c6ggBIjzKT7XR5FIB53HHOhnsT7a0Cc-PpneWq9zX&source=gbs_api")' }}></div>
                       <div className="book-shelf-changer">
                         <select>
                           <option value="none" disabled>Move to...</option>
@@ -21,14 +21,14 @@ class CurrentlyReading extends Component {
                         </select>
                       </div>
                     </div>
-                    <div className="book-title">To Kill a Mockingbird</div>
-                    <div className="book-authors">Harper Lee</div>
+                    <div className="book-title">1776</div>
+                    <div className="book-authors">David McCullough</div>
                   </div>
                 </li>
                 <li>
                   <div className="book">
                     <div className="book-top">
-                      <div className="book-cover" style={{ width: 128, height: 188, backgroundImage: 'url("http://books.google.com/books/content?id=yDtCuFHXbAYC&printsec=frontcover&img=1&zoom=1&imgtk=AFLRE72RRiTR6U5OUg3IY_LpHTL2NztVWAuZYNFE8dUuC0VlYabeyegLzpAnDPeWxE6RHi0C2ehrR9Gv20LH2dtjpbcUcs8YnH5VCCAH0Y2ICaKOTvrZTCObQbsfp4UbDqQyGISCZfGN&source=gbs_api")' }}></div>
+                      <div className="book-cover" style={{ width: 128, height: 192, backgroundImage: 'url("http://books.google.com/books/content?id=wrOQLV6xB-wC&printsec=frontcover&img=1&zoom=1&imgtk=AFLRE72G3gA5A-Ka8XjOZGDFLAoUeMQBqZ9y-LCspZ2dzJTugcOcJ4C7FP0tDA8s1h9f480ISXuvYhA_ZpdvRArUL-mZyD4WW7CHyEqHYq9D3kGnrZCNiqxSRhry8TiFDCMWP61ujflB&source=gbs_api")' }}></div>
                       <div className="book-shelf-changer">
                         <select>
                           <option value="none" disabled>Move to...</option>
@@ -39,8 +39,8 @@ class CurrentlyReading extends Component {
                         </select>
                       </div>
                     </div>
-                    <div className="book-title">Ender's Game</div>
-                    <div className="book-authors">Orson Scott Card</div>
+                    <div className="book-title">Harry Potter and the Sorcerer's Stone</div>
+                    <div className="book-authors">J.K. Rowling</div>
                   </div>
                 </li>
               </ol>
@@ -50,4 +50,4 @@ class CurrentlyReading extends Component {
     }
 }
 
-export default CurrentlyReading
+export default WantToRead
