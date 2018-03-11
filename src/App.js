260b77d7904a1e9d80f17b7bf060f397df0d284e
Shannon <13708207+smckinney1@users.noreply.github.com>
2018-03-11 17:03:48 -0400

    Implemented React Router to go back and forth between the main page and the Search page. Also began separating out components by implementing Search.js component.

--- src/App.js
+++ src/App.js
@@ -1,8 +1,10 @@
-import React from 'react'
+import React, { Component } from 'react'
+import { Route, Link } from 'react-router-dom'
 // import * as BooksAPI from './BooksAPI'
+import Search from './Search'
 import './App.css'
 
-class BooksApp extends React.Component {
+class BooksApp extends Component {
   state = {
     /**
      * TODO: Instead of using this state variable to keep track of which page
@@ -16,28 +18,10 @@ class BooksApp extends React.Component {
   render() {
     return (
       <div className="app">
-        {this.state.showSearchPage ? (
-          <div className="search-books">
-            <div className="search-books-bar">
-              <a className="close-search" onClick={() => this.setState({ showSearchPage: false })}>Close</a>
-              <div className="search-books-input-wrapper">
-                {/*
-                  NOTES: The search from BooksAPI is limited to a particular set of search terms.
-                  You can find these search terms here:
-                  https://github.com/udacity/reactnd-project-myreads-starter/blob/master/SEARCH_TERMS.md
-
-                  However, remember that the BooksAPI.search method DOES search by title or author. So, don't worry if
-                  you don't find a specific author or title. Every search is limited by search terms.
-                */}
-                <input type="text" placeholder="Search by title or author"/>
-
-              </div>
-            </div>
-            <div className="search-books-results">
-              <ol className="books-grid"></ol>
-            </div>
-          </div>
-        ) : (
+        <Route path="/search" render={({ history }) => (
+          <Search />
+        )} />
+        <Route exact path="/" render={({history}) => (
           <div className="list-books">
             <div className="list-books-title">
               <h1>MyReads</h1>
@@ -194,10 +178,11 @@ class BooksApp extends React.Component {
               </div>
             </div>
             <div className="open-search">
-              <a onClick={() => this.setState({ showSearchPage: true })}>Add a book</a>
+              {/* <a onClick={() => this.setState({ showSearchPage: true })}>Add a book</a> */}
+              <Link to="/search">Search</Link>
             </div>
           </div>
-        )}
+          )} />
       </div>
     )
   }
