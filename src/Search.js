f6b8c9c9d294351499fe2f079bb2fba89a7100aa
Shannon <13708207+smckinney1@users.noreply.github.com>
2018-03-13 20:51:57 -0400

    Bookshelf selection defaults to None when in Search mode.

--- src/Search.js
+++ src/Search.js
@@ -32,7 +32,7 @@ class Search extends Component {
                                 <div className="book">
                                     <div className="book-top">
                                         <div className="book-cover" style={{ width: 128, height: 192, backgroundImage: `url(${book.imageLinks.thumbnail}` }}></div>
-                                        <BookMoveOptions />
+                                        <BookMoveOptions shelf="none"/>
                                     </div>
                                     <div className="book-title">{book.title}</div>
                                     <div className="book-authors">{book.authors}</div>
